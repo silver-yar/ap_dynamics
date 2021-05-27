922b15be22044b3399b9133da3c1eae082be162f
Johnathan Handy <42393941+silver-yar@users.noreply.github.com>
2021-05-27 19:39:40 -0400

    implemented catch2 testing in CMakeLists.txt

---
+++ Tests/temp-test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "../DSP/APOverdrive.h"
+
+TEST_CASE("DSP Soft Clipping Test")
+{
+  APOverdrive overdrive;
+  auto sample = 1.0f;
+  auto sampleOriginCopy = sample;
+  overdrive.process(&sample, 1.0f, &sample, 1);
+  REQUIRE(sampleOriginCopy != sample);
+}
