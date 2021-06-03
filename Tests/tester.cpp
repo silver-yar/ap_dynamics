f7c72144fcd091ae74c9c582cf9aaf3f7d9c942f
Johnathan Handy <42393941+silver-yar@users.noreply.github.com>
2021-06-03 17:23:29 -0400

    added compiler warnings as errors

--- Tests/tester.cpp
+++ Tests/tester.cpp
@@ -2,11 +2,22 @@
 #include <catch2/catch.hpp>
 #include "APOverdrive.h"
 
-TEST_CASE("DSP Soft Clipping Test")
+TEST_CASE("DSP Overdrive Test")
 {
   APOverdrive overdrive;
-  auto sample = 1.0f;
-  auto sampleOriginCopy = sample;
-  overdrive.process(&sample, 1.0f, &sample, 1);
-  REQUIRE(sampleOriginCopy != sample);
+  auto sampleArray = std::vector<float> {0.0f, 0.2f, 0.4f, 0.6f, 0.8f, 1.0f};
+  std::vector<float> resultArray(sampleArray.size());
+  float mix = 0.0f;
+
+  SECTION("No Clipping") {
+
+  }
+
+  SECTION("Soft Clipping") {
+    mix = 0.4f;
+  }
+
+  SECTION("Hard Clipping") {
+    mix = 0.8f;
+  }
 }
